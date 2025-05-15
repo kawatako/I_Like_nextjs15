55a7dede5f0d6f12ac223397ecb527920be4507d
kawatashota <qhqn_hqnd@icloud.com>
2025-05-15 13:43:17 +0900

    平均順位の集計ロジック変更

--- app/trends/average/[subject]/page.tsx
+++ app/trends/average/[subject]/page.tsx
@@ -1,5 +1,4 @@
 // app/trends/average/[subject]/page.tsx
-//サーバー上で URL から「何のテーマ」のリクエストかをデコードして取得し、見出しと、テーマ名を渡した子コンポーネントを描画し、子コンポーネント側で初めてクライアント（ブラウザ）でデータを取りにいって表示
 import AverageItemRankList from "@/components/component/trends/AverageItemRankList";
 
 interface PageProps {
@@ -8,16 +7,16 @@ interface PageProps {
 
 export default async function SubjectAveragePage({ params }: PageProps) {
   const { subject: rawSubject } = await params;
-
-  // URL エンコードされているかもしれないのでデコード
   const subject = Array.isArray(rawSubject)
-    ? decodeURIComponent(rawSubject[0]) // 配列なら最初の要素を取り出す
-    : decodeURIComponent(rawSubject); // 文字列ならそのまま使う
+    ? decodeURIComponent(rawSubject[0])
+    : decodeURIComponent(rawSubject);
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">「{subject}」のランキング集計</h1>
-      <h3 className="text-lg mb-4">集計結果は毎日0時に更新されます</h3>
+      <h1 className="text-2xl font-bold mb-4">
+        「{subject}」のボルダ集計ランキング
+      </h1>
+      <h3 className="text-lg mb-4">スコアは毎日0時に更新されます</h3>
       <AverageItemRankList subject={subject} />
     </div>
   );
